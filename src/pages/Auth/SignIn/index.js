3e0c2343696f40d00bcf66b67b443ab295ae90e0
Juliano <julianotrabalho2016@gmail.com>
2019-10-03 21:03:04 -0300

    UPDATE - login page

--- src/pages/Auth/SignIn/index.js
+++ src/pages/Auth/SignIn/index.js
@@ -1,7 +1,20 @@
 import React from "react";
+import Button from "../../../styles/components/Button";
+import { Container, SigninForm } from "../styles";
 
-// import { Container } from './styles';
-
-const SignIn = () => <div />;
+const SignIn = () => (
+  <Container>
+    <SigninForm>
+      <h1>Bem Vindo</h1>
+      <span>E-MAIL</span>
+      <input type="email" name="email" />
+      <span>SENHA</span>
+      <input type="password" name="password" />
+      <Button type="submit" size="big">
+        Entrar
+      </Button>
+    </SigninForm>
+  </Container>
+);
 
 export default SignIn;
